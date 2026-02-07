307b8f7403de24e77468215cec5e6aed872bdaa5
GJ Vlok <gerhardvlok21@gmail.com>
2026-02-07 21:24:31 +0200

    made get_theme function in theme.py

---
+++ ui/utils/theme.py
@@ -0,0 +1,12 @@
+# utils/theme.py
+import flet as ft
+
+def get_theme(mode="dark"):
+    return ft.Theme(
+        color_scheme=ft.ColorScheme(
+            primary="#afdaaf", # Green for positive feedback
+            error="red",
+            background="#1e1e2e" if mode == "dark" else "white",
+        ),
+        use_material3=True, # Modern look
+    )
